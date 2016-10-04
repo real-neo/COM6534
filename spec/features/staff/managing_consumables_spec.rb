9851c5fbcbd92527e1972002a4b971ad77ae11d6
Ryan Bibby <r.bibby@sheffield.ac.uk>
2016-10-04 15:12:50 +0100

    Seeds and test outline

---
+++ spec/features/staff/managing_consumables_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'Managing consumables' do
+
+  context 'As a staff member', js: true do
+    
+    specify 'I can create a new consumable' do
+      skip 'Implement me'
+    end
+
+    context 'Given that a consumable already exists' do
+
+      specify 'I can edit the stock level for an item' do
+        skip 'Implement me'
+      end
+
+    end 
+
+  end
+
+end
