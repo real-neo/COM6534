00150ac2e2c2bd606b5cde5d3eb9823fd14ba113
James Gregory <james.gregory@epigenesys.org.uk>
2016-10-04 12:06:58 +0100

    Changes to factories

--- spec/factories/consumables.rb
+++ spec/factories/consumables.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :consumable do
     name "MyString"
     description "MyString"
-    stock_level 1
-    min_stock_level 1
+    stock_level 500
+    min_stock_level 10
     category :chemical
   end
 end
