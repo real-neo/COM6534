eca99013b5b982dec6f8381a3876f1704c1bd363
Bowen Yu <byu8@sheffield.ac.uk>
2018-12-03 00:58:32 +0000

    Fix deploy bug.

--- db/schema.rb
+++ db/schema.rb
@@ -12,48 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_11_29_213630) do
 
-  create_table "accounts", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_accounts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
-  end
-
-  create_table "consumables", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "stock_level"
-    t.integer "min_stock_level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "category"
-  end
-
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
-
   create_table "requirements", force: :cascade do |t|
     t.string "company_name"
     t.string "email"
