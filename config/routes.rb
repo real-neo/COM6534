1805b65c9540670f142cb06a713583963f398d49
Bowen Yu <byu8@sheffield.ac.uk>
2018-12-06 18:57:08 +0000

    Friendly error pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :mods
+  match '/403', to: "errors#error_403", via: :all
+  match '/404', to: 'errors#error_404', via: :all
+  match '/422', to: 'errors#error_422', via: :all
+  match '/500', to: 'errors#error_500', via: :all
+
+  get :ie_warning, to: 'errors#ie_warning'
+  get :javascript_warning, to: 'errors#javascript_warning'
+
   mount EpiCas::Engine, at: "/"
   devise_for :users
   resources :requirements do
@@ -13,8 +20,9 @@ Rails.application.routes.draw do
     end
   end
   resources :users
+  resources :mods
   resources :records
   get 'dashboard', to: 'records#read_records'
-  root 'welcome#index'
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
