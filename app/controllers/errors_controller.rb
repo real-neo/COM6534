d71fbe7c51e7b7f6373d227fee03b1aaa8d0f07f
Bowen Yu <byu8@sheffield.ac.uk>
2018-12-06 19:13:16 +0000

    Guest can see error pages.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
-
+  skip_before_action :authenticate_user!
   skip_before_action :ie_warning
   skip_before_action :verify_authenticity_token, only: [:error_422]
 
