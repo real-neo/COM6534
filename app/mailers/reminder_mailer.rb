bb156d7185e2ea6e6bda27b0ce5d979eefe109d7
Bowen Yu <byu8@sheffield.ac.uk>
2018-12-07 23:07:06 +0000

    Delete useless code.

--- app/mailers/reminder_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ReminderMailer < ApplicationMailer
-
-  def low_stock_level(letter_generation_id)
-    @letter_generation = LetterGeneration.find(letter_generation_id)
-    @account = @letter_generation.account.decorate
-
-    mail to: @letter_generation.account.mail, subject: "The letters you have requested are ready for download"
-  end
-end
